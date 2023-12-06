eac08ff806f39f06af0cd1365a5f527453454f5f
Cate <ckingsley726@gmail.com>
2023-12-06 12:22:52 -0500

    add velocity msg copying

--- src/robot_no_crash_node.cpp
+++ src/robot_no_crash_node.cpp
@@ -5,12 +5,15 @@
 
 #include <sstream>
 
-void lidarCallback(){
-  ;
+sensor_msgs::LaserScan lidar_msg;
+
+void lidarCallback(const sensor_msgs::LaserScan msg){
+  lidar_msg = msg;
 }
 
-void velocityCallback(){
-  ;
+void velocityCallback(const geometry_msgs::Twist msg){
+  vel_msg = msg;
+  p_pub->publish(vel_msg);
 }
 
 int main(int argc, char **argv)
@@ -20,8 +23,6 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "robot_no_crash");
 
   ros::NodeHandle n;
-  
-  
 
   ros::Publisher no_crash_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
   p_pub = &no_crash_pub;
@@ -31,9 +32,9 @@ int main(int argc, char **argv)
 
   ros::Rate loop_rate(10);
 
-    geometry_msgs::Twist msg;
+    geometry_msgs::Twist vel_msg;
     
-    no_crash_pub.publish(msg);
+    no_crash_pub.publish(vel_msg);
 
   
   return 0;
